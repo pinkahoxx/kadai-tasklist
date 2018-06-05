1efba174b8ecae0303ad68e9113403776e95eaf2
kaho.yoshii <ec2-user@ip-172-31-27-95.us-east-2.compute.internal>
2018-06-05 06:57:55 +0000

    add col-md

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -20,8 +20,8 @@
     </table>
 
 
- {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id])], ['class' => 'btn btn-default']) !!}
- <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+ {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+ <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
