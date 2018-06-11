a413dbd9353f94b5263a420ce2edcb3d2b0ee688
kaho.yoshii <ec2-user@ip-172-31-27-95.us-east-2.compute.internal>
2018-06-11 03:35:08 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'TasksController@index');
+Route::get('/', function(){
+    return view('welcome');
+});
 
 Route::resource('tasks','TasksController');
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
